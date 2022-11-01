fbd31f7695906113edc84ed65d5ba42caa95a3a2
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-01 14:45:31 -0600

    added icon to drag roomate component and styled it

--- src/components/Rommate.js
+++ src/components/Rommate.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 
 function Roomate({ photoUrl, name }) {
   return (
     <div className='roomate'>
+      <DragIndicatorIcon />
+
       <img src={photoUrl} alt={`${name} profile`} />
 
-      <h4>{name}</h4>
+      <div>
+        <h4>{name}</h4>
+        <p>Last time thrown:</p>
+      </div>
     </div>
   );
 }
