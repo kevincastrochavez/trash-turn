649c3ed1d7008d8671572ad117e4c960d35f68ec
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-02 14:34:01 -0600

    protected the root route in case there is no user logged in

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+import { useStateValue } from '../StateProvider';
 import Login from './Login';
 import RommateInfo from './RoomateInfo';
 import RommateRole from './RoomateRole';
 
 function Layout() {
+  const [{ fullUser }] = useStateValue();
+
   return (
     <div className='layout'>
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<RommateRole />} />
+          <Route
+            exact
+            path='/'
+            element={
+              fullUser === null ? <Navigate to='/login' /> : <RommateRole />
+            }
+          />
           <Route path='/login' element={<Login />} />
           <Route path='/rommateInfo' element={<RommateInfo />} />
         </Routes>
